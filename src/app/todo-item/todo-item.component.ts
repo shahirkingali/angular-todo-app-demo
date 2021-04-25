99753ae0106a0ca89231071cb9f3ee299c4fe20b
Shahir Ali <shahirsheikali@gmail.com>
2021-04-25 10:11:44 -0700

    first commit

---
+++ src/app/todo-item/todo-item.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Todo } from '../shared/todo.model'
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.scss']
+})
+export class TodoItemComponent implements OnInit {
+
+  @Input() todo: Todo
+  @Output() todoClicked: EventEmitter<void> = new EventEmitter()
+  @Output() editClicked: EventEmitter<void> = new EventEmitter()
+  @Output() deleteClicked: EventEmitter<void> = new EventEmitter()
+
+  constructor() { }
+
+  ngOnInit(): void {
+ 
+  }
+
+
+  onTodoClicked() {
+    this.todoClicked.emit()
+  }
+
+  onEditClciked() {
+    this.editClicked.emit()
+  }
+
+  onDeleteClicked () {
+    this.deleteClicked.emit()
+  }
+
+}
+
+
